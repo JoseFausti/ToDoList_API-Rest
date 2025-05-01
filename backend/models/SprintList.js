288311f6c49a1f981dddccbcff463109c8c3bf5b
JoseFausti <joseemilianofausti@gmail.com>
2025-05-01 14:09:54 -0300

    new

--- backend/models/SprintList.js
+++ backend/models/SprintList.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
+const taskSchema = require("./Task");
 
 const sprintSchema = new mongoose.Schema({
     nombre: { type: String, required: true },
-    fechaInicio: { type: Date, required: true },
-    fechaCierre: { type: Date, required: true },
-    tareas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task', default: [] }],
+    fechaInicio: { type: String, required: true },
+    fechaCierre: { type: String, required: true },
+    tareas: [taskSchema],
     // color: { type: String},
 });
 
--- backend/models/Task.js
+++ backend/models/Task.js
@@ -4,7 +4,7 @@ const taskSchema = new mongoose.Schema({
     titulo: {type: String, required: true},
     descripcion: {type: String},
     estado: {type: String, enum: ['pendiente', 'en progreso', 'completado'], required: true},
-    fechaLimite: {type: Date, required: true},
+    fechaLimite: {type: String, required: true},
     // color: {type: String}
 });
 
